9348f6747596f77846fa3566cde963231b6d02ec
MartinMa <maang_ouc@163.com>
2018-12-20 18:49:10 +0800

    override equals and hashcode

--- src/com/screwcode/Person.java
+++ src/com/screwcode/Person.java
@@ -1,5 +1,7 @@
 package com.screwcode;
 
+import java.util.Objects;
+
 // 修改Person的实现，使得CommonElementsInObjectListTest测试通过
 public class Person {
     private final String name;
@@ -10,5 +12,21 @@ public class Person {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) return true;
+        if (o != null && o instanceof Person) {
+            Person person = (Person) o;
+            return Objects.equals(this.name, person.name);
+        }
+        return false;
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(this.name);
     }
 }

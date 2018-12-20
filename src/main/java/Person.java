542349a7283279f9ff7c089f9fef8d1735fe00ec
Bo Zhang <bo@gradle.com>
2018-12-20 20:28:56 +0800

    Mavenize project

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,5 +1,3 @@
-package com.screwcode;
-
 public class Person {
     /**
      * 身份证号
---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+
+import java.util.HashSet;
+
+import static org.junit.Assert.*;
+
+public class PersonTest {
+    @Test
+    public void testEquals() {
+        assertEquals(new Person("1"), new Person("1"));
+        assertNotEquals(new Person("1"), new Person("2"));
+
+        Person person = new Person("1");
+        person.setAge(20);
+
+        assertEquals(person, new Person("1"));
+    }
+
+    @Test
+    public void testContains() {
+        Person person = new Person("1");
+        HashSet<Person> set = new HashSet<>();
+        set.add(person);
+
+        assertTrue(set.contains(person));
+        person.setName("张三");
+        assertTrue(set.contains(person));
+    }
+}

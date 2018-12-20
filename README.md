da40d899825d71c151c6f66fa30b8d642c3fc522
Bo Zhang <bo@gradle.com>
2018-12-20 09:23:31 +0800

    FixCompareBug

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 [回到首页](https://github.com/screwcode/JavaCollection)
 
-## 修复Bug: 错误的hashCode实现
+## 修复Bug: 错误的compare实现
 
 小明写了一个类，但是测试没有通过。
 
-请修复`src/com/screwcode/Person.java`中的bug，使得测试通过（`src/com/screwcode/PersonTest.main()`没有异常产生）然后提交Pull Request。
-切记，Pull Request的base分支一定要选择`FixHashCodeBug`！
+请修复`src/com/screwcode/Point.java`中的bug，使得测试通过（`src/com/screwcode/PointTest.main()`没有异常产生）然后提交Pull Request。
+切记，Pull Request的base分支一定要选择`FixCompareBug`！
---
+++ src/com/screwcode/Point.java
@@ -0,0 +1,58 @@
+package com.screwcode;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * 笛卡尔坐标系中的一个点
+ */
+public class Point {
+    private final int x;
+    private final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Point point = (Point) o;
+
+        if (x != point.x) {
+            return false;
+        }
+        return y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
+
+    public static List<Point> sort(List<Point> list) {
+        List<Point> ret = new ArrayList<>(list);
+        ret.sort(new Comparator<Point>() {
+            @Override
+            public int compare(Point p1, Point p2) {
+                // 按照先x再y，从小到大的顺序排序
+                if (p1.x != p2.x) {
+                    return p1.x - p2.x;
+                } else {
+                    return p1.y - p2.y;
+                }
+            }
+        });
+        return ret;
+    }
+}

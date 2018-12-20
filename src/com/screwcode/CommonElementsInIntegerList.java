1a21816e91d486edbbd8979b70627f2c4dd989ad
Bo Zhang <bo@gradle.com>
2018-12-20 19:51:00 +0800

    Revert "commit for pr"
    This reverts commit 96c8c85fe579a1b9d8aa350525d3292c54fa511b.

--- src/com/screwcode/CommonElementsInIntegerList.java
+++ src/com/screwcode/CommonElementsInIntegerList.java
@@ -1,12 +1,9 @@
 package com.screwcode;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class CommonElementsInIntegerList {
     public static Set<Integer> commonElementsIn(List<Integer> a, List<Integer> b) {
         // 在这里写你自己的实现
-        Set<Integer> result = a.stream().filter(t -> b.contains(t)).collect(Collectors.toSet());
-        return result;
     }
 }

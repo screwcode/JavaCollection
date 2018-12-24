3150945d3d98c72a80be9ccb237ae0150930ecd0
Bo Zhang <bo@gradle.com>
2018-12-24 20:19:24 +0800

    Add a test case to avoid ambiguity

--- src/main/java/RemoveDuplicateCharsInString.java
+++ src/main/java/RemoveDuplicateCharsInString.java
@@ -4,6 +4,7 @@ public class RemoveDuplicateCharsInString {
     // 修改这个方法使得它能够输出正确结果：
     // 例如，输入aabbcc返回abc
     // 输入ccbbaa返回cba
+    // 输入apple返回aple
     public static String removeDuplicateCharsInString(String s) {
         HashSet<Character> charSet = new HashSet<>();
         for (int i = 0; i < s.length(); i++) {

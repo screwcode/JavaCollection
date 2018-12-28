75abc87aafadfcf3740f00a14457ea59ea31fa0b
Bo Zhang <bo@gradle.com>
2018-12-29 07:02:09 +0800

    Revert "寻找两个整数List的公共元素 (#9)"
    This reverts commit 0d1b8e3ed3ad36bcd00a1bb2119138aea43ece65.

--- src/main/java/CommonElementsInIntegerList.java
+++ src/main/java/CommonElementsInIntegerList.java
@@ -1,13 +1,8 @@
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class CommonElementsInIntegerList {
     public static Set<Integer> commonElementsIn(List<Integer> a, List<Integer> b) {
-        Set<Integer> s1 = new HashSet<>(a);
-        Set<Integer> s2 = new HashSet<>(b);
-        s1.retainAll(s2);
-        return s1;
+        // 在这里写你自己的实现
     }
 }

0d1b8e3ed3ad36bcd00a1bb2119138aea43ece65
Loadingxyz123 <40271830+Loadingxyz123@users.noreply.github.com>
2018-12-29 07:01:08 +0800

    寻找两个整数List的公共元素 (#9)

--- src/main/java/CommonElementsInIntegerList.java
+++ src/main/java/CommonElementsInIntegerList.java
@@ -1,8 +1,13 @@
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class CommonElementsInIntegerList {
     public static Set<Integer> commonElementsIn(List<Integer> a, List<Integer> b) {
-        // 在这里写你自己的实现
+        Set<Integer> s1 = new HashSet<>(a);
+        Set<Integer> s2 = new HashSet<>(b);
+        s1.retainAll(s2);
+        return s1;
     }
 }

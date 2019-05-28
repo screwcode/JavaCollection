205a15cb1c5285fdaae348ae3a6cfdf3e337c840
Bo Zhang <bo@gradle.com>
2019-05-28 17:05:31 +0800

    Revert "修改了Person.java 中的代码。 (#20)"
    This reverts commit d166f594d43dadde8cf073f6fd37b85d95b82b39.

--- src/main/java/CommonElementsInObjectList.java
+++ src/main/java/CommonElementsInObjectList.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
 public class CommonElementsInObjectList {
-
     public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {
         Set<Person> setA = new HashSet<>(a);
         Set<Person> setB = new HashSet<>(b);
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -9,14 +9,4 @@ public class Person {
     public String getName() {
         return name;
     }
-
-    @Override
-    public int hashCode() {
-        return name.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return this.hashCode() == obj.hashCode();
-    }
 }

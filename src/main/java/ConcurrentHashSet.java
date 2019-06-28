d4940aa86843c719dc1fde01780fe6462136edbe
JayChenFE <chenjiehwy@163.com>
2019-06-28 23:50:38 +0800

    修复ConcurrentHashSet在并发环境下的问题

--- src/main/java/ConcurrentHashSet.java
+++ src/main/java/ConcurrentHashSet.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -5,13 +6,13 @@ import java.util.Set;
  * 一个在多线程环境下也能安全的添加元素的HashSet
  */
 public class ConcurrentHashSet {
-    private Set<Object> elements = new HashSet<>();
+    private Set<Object> safeElements = Collections.synchronizedSet(new HashSet<>());
 
     public void add(Object i) {
-        elements.add(i);
+        safeElements.add(i);
     }
 
     public int size() {
-        return elements.size();
+        return safeElements.size();
     }
 }

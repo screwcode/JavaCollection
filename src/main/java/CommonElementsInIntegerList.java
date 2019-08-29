21a793eff2438cb7f5728d2547f672455be29e26
marble <1218892028@qq.com>
2019-08-29 14:01:24 +0800

    比较两个集合相同元素

--- src/main/java/CommonElementsInIntegerList.java
+++ src/main/java/CommonElementsInIntegerList.java
@@ -1,8 +1,18 @@
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class CommonElementsInIntegerList {
     public static Set<Integer> commonElementsIn(List<Integer> a, List<Integer> b) {
         // 在这里写你自己的实现
+        Set<Integer> set = new HashSet<>(a);
+        Set<Integer> result = new HashSet<>();
+        for(int i = 0;i<b.size();i++){
+            int tmp=b.get(i);
+            if(!set.add(tmp)){
+                result.add(tmp);
+            };
+        }
+        return result;
     }
 }

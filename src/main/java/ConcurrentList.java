3e6af3832814984b5b0010cba78bec58acbeaff7
SaberXu <xushibo@annetinfo.com>
2019-10-11 18:36:17 +0800

    1、给集合对象的所有操作方法加同步锁 2、使用Collections.synchronizedList()方法，返回一个线程安全的List

--- src/main/java/ConcurrentList.java
+++ src/main/java/ConcurrentList.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,7 +6,11 @@ import java.util.List;
  * 一个在多线程环境下也能安全的添加元素的List
  */
 public class ConcurrentList {
-    private List<Integer> elements = new ArrayList<>();
+    private  List<Integer> elements = Collections.synchronizedList(new ArrayList<>());
+
+//    public synchronized void add(Integer i) {
+//        elements.add(i);
+//    }
 
     public void add(Integer i) {
         elements.add(i);

1fbc33067f28b80e9c510fbe338a5c48f651410e
SaberXu <xushibo@annetinfo.com>
2019-10-12 09:47:00 +0800

    1、使用Collections.synchronizedSet()方法，返回一个线程安全的HashSet 2、CI 校验jdk11

--- src/main/java/ConcurrentHashSet.java
+++ src/main/java/ConcurrentHashSet.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -5,7 +6,7 @@ import java.util.Set;
  * 一个在多线程环境下也能安全的添加元素的HashSet
  */
 public class ConcurrentHashSet {
-    private Set<Object> elements = new HashSet<>();
+    private Set<Object> elements = Collections.synchronizedSet(new HashSet<>());
 
     public void add(Object i) {
         elements.add(i);

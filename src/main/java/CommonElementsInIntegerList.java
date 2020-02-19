2137771d9c191ef1580751512260d149d3dbea0e
aichiwuhuarou <2862942216@qq.com>
2020-02-19 17:14:25 +0800

    提交的求两个整数List的交集

--- src/main/java/CommonElementsInIntegerList.java
+++ src/main/java/CommonElementsInIntegerList.java
@@ -1,8 +1,20 @@
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class CommonElementsInIntegerList {
     public static Set<Integer> commonElementsIn(List<Integer> a, List<Integer> b) {
         // 在这里写你自己的实现
+        Set<Integer> resuleSet = new HashSet<>();
+
+        for(Integer c: a){
+            if(b.contains(c)){
+               resuleSet.add(c);
+            }
+
+        }
+
+        return resuleSet;
     }
 }

844b4bdd6e8b518441a60fc840fcdcf83345aab1
liaofeng <liaofeng1024@163.com>
2020-05-08 14:18:22 +0800

    使用LinkedHashSet保证插入顺序

--- src/main/java/RemoveDuplicateCharsInString.java
+++ src/main/java/RemoveDuplicateCharsInString.java
@@ -1,4 +1,6 @@
 import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
 
 public class RemoveDuplicateCharsInString {
     // 修改这个方法使得它能够输出正确结果：
@@ -6,7 +8,7 @@ public class RemoveDuplicateCharsInString {
     // 输入ccbbaa返回cba
     // 输入apple返回aple
     public static String removeDuplicateCharsInString(String s) {
-        HashSet<Character> charSet = new HashSet<>();
+        LinkedHashSet<Character> charSet = new LinkedHashSet<>();
         for (int i = 0; i < s.length(); i++) {
             charSet.add(s.charAt(i));
         }

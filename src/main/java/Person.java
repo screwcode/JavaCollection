26309eef3edc3d0b6b73ae33bd72aecd1bd841ca
dingw <dingweibing@outlook.jp>
2020-06-01 14:06:01 +0900

    my commit

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,9 +1,24 @@
+import java.util.Objects;
+
 // 修改Person的实现，使得CommonElementsInObjectListTest测试通过
 public class Person {
     private final String name;
 
     public Person(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 
     public String getName() {

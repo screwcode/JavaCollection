4737847ffcf70f2b98ba2cfd7f30d7b79042dccb
catmmao <catmmao@gmail.com>
2021-03-11 18:07:00 +0800

    如果改变 keySet 返回的 Set 集合，原 Map 也会被改变

--- src/main/java/CharCount.java
+++ src/main/java/CharCount.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -44,8 +45,9 @@ public class CharCount {
     public int howManyCharsInCommon(CharCount anotherCharCount) {
         Set<Character> myChars = chars();
         Set<Character> theirChars = anotherCharCount.chars();
+        Set<Character> result = new HashSet<>(theirChars);
 
-        theirChars.retainAll(myChars);
-        return theirChars.size();
+        result.retainAll(myChars);
+        return result.size();
     }
 }

5436cdc70b1b8cd0e7694daeb1dc95c6cb46840a
catmmao <catmmao@gmail.com>
2021-03-11 17:53:16 +0800

    值和实际的class都一致才相等

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -7,16 +7,8 @@ public class Person {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        Person person = (Person) o;
-
-        return id == person.id;
+        Person p = (Person) o;
+        return (id.equals(p.id)) && (this.getClass() == o.getClass());
     }
 
     @Override
